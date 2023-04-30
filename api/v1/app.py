2ddc644431719c955d6ab8063b615deed09837af
Bolexzy <boluoyewumi79@gmail.com>
2023-04-30 17:14:27 +0100

    Update: stash apply

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
 """
-This module contains a simple flask application
+Flask App that integrates AirBnB static HTML Template
 """
-from flask import Flask, jsonify, make_response
-from flask_cors import CORS
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+import os
 
+# Set flask server environmental setup
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
 
-host = getenv("HBNB_API_HOST")
-port = getenv("HBNB_API_PORT")
 app = Flask(__name__)
+# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": '0.0.0.0'}})
-
-
-@app.errorhandler(404)
-def error_handler(error):
-    """handle 404 errors"""
-    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """handle exit"""
+def teardown(exception):
+    """Close the storage session after each request."""
     storage.close()
 
 
+@app.errorhandler(404)
+def handle_api_error(exception):
+    """Returns a JSON-formatted 404 status code response."""
+    return make_response(jsonify({
+                    "error": "Not found"
+                    }), 404)
+
+
 if __name__ == "__main__":
-    if not host:
-        host = "0.0.0.0"
-    if not port:
-        port = 5000
-    app.run(host=host, port=port, threaded=True, debug=True)
+    # start Flask app
+    app.run(host=host, port=port, threaded=True)
