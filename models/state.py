44036d4e4cb3de5b16ff15e30904802b2a83f6d1
Bolexzy <boluoyewumi79@gmail.com>
2023-04-30 16:17:37 +0100

    Update: fix cascade error

--- models/state.py
+++ models/state.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-""" holds class State"""
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
+""" State Module for HBNB project """
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
+import models
+from models.city import City
 
 
 class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
+    """Represents a state for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
 
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
+    __tablename__ = 'states'
 
-    if models.storage_t != "db":
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="all, delete")
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """getter for list of city instances related to the state"""
+            """Getter method for cities
+            Return: list of cities with state_id equal to self.id
+            """
             city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
+
+            for city in list(models.storage.all(City).values()):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
